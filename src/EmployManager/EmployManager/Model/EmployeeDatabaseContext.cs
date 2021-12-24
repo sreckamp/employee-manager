de4c0b16c9fcdd2a4859d484855641cb567d7230
Steve Reckamp <sreckamp@gmail.com>
2021-12-23 19:51:59 -0600

    Create backing data model and database interface.

---
+++ src/EmployManager/EmployManager/Model/EmployeeDatabaseContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployManager.Model
+{
+    public class EmployeeDatabaseContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=Employees.db", options =>
+            {
+                options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().ToTable("employees", "employee-manager");
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasKey(employee => employee.Id);
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/EmployManager/EmployManager/Model/SqLiteEmployeeManager.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace EmployManager.Model
+{
+    /// <summary>
+    /// Employee manager that utilizes SqLite as the backing store.
+    /// </summary>
+    public class SqLiteEmployeeManager : IEmployeeManager, IDisposable
+    {
+        private readonly EmployeeDatabaseContext _context = new();
+        private bool _disposed;
+
+        /// <inheritdoc/>
+        public IEnumerable<Employee> Employees { get; }
+
+        public SqLiteEmployeeManager()
+        {
+            Employees = _context.Employees;
+        }
+        
+        /// <inheritdoc/>
+        public void Initialize()
+        {
+            _context.Database.EnsureCreated();
+            if (_context.Employees.Any()) return;
+
+            Debug.WriteLine("Loading defaults");
+            Add("Co-Manager", "Jim Halpert", "images/jim.png");
+            Add("Co-Manager", "Michael Scott", "images/michael.png");
+            Add("Secretary", "Erin Hannon", "images/erin.jpg");
+            Add("Customer Service", "Kelly Kapoor");
+            Add("Sales", "Pam Halpert", "images/pam.jpg");
+            Add("Sales", "Stanley Hudson");
+            Add("Sales", "Phyllis Vance");
+            Add("Sales", "Dwight Bratton");
+            Add("Sales", "Andy Bernard", "images/andy.jpg");
+            Add("Quality Assurance", "Creed Bratton");
+            Add("Human Resources", "Toby Flenderson");
+            Add("Accounting", "Angela Martin", "images/angela.jpg");
+            Add("Accounting", "Kevin Malone");
+            Add("Accounting", "Oscar Martinez");
+            Add("Temp", "Ryan Howard", "images/ryan.png");
+        }
+
+        /// <inheritdoc/>
+        public Employee Add(string title, string name, string imagePath = null)
+        {
+            CheckDisposed();
+            byte[] imageData = null;
+            if (imagePath != null && File.Exists(imagePath))
+            {
+                imageData = File.ReadAllBytes(imagePath);
+            }
+            var newEmployee = new Employee {JobTitle = title, Name = name, Image = imageData};
+            _context.Add(newEmployee);
+            _context.SaveChanges();
+            return newEmployee;
+        }
+
+        /// <inheritdoc/>
+        public void Delete(Employee employee)
+        {
+            CheckDisposed();
+            _context.Remove(employee);
+            _context.SaveChanges();
+        }
+
+        /// <inheritdoc/>
+        public void Save(Employee employee)
+        {
+            CheckDisposed();
+            _context.SaveChanges();
+        }
+
+        private void CheckDisposed()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(GetType().ShortDisplayName(), CoreStrings.ContextDisposed);
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            if (_disposed) return;
+            _context.Dispose();
+            _disposed = true;
+        }
+    }
+}
